085c0c22ab562036013dce24fc495b32bd00dcaa
Alexandre Guérin <alexandre.guerin17@gmail.com>
2022-08-21 22:52:59 +0200

    Global test

--- TypeScript/test/golden-master-text-test.ts
+++ TypeScript/test/golden-master-text-test.ts
@@ -1,16 +1,22 @@
-import { Objet, RoseDoree } from '../app/rose-doree';
+import { Conjure } from '../app/models/conjure';
+import { Objet } from '../app/models/objet';
+import { ObjetRoseDoree } from '../app/models/objetRoseDoree';
+import { PassCoulisseMetallica } from '../app/models/passCoulisseMetallica';
+import { Sulfura } from '../app/models/sulfura';
+import { VieuxBrie } from '../app/models/vieuxBrie';
+import { RoseDoree } from '../app/rose-doree';
 
 const objets = [
-  new Objet("Veste de dexterite +5", 10, 20), //
-  new Objet("Vieux Brie", 2, 0), //
-  new Objet("Elixir de la Mangouste", 5, 7), //
-  new Objet("Sulfuras, Main de Ragnaros", 0, 80), //
-  new Objet("Sulfuras, Main de Ragnaros", -1, 80),
-  new Objet("Pass pour les coulisses d'un concert de Metallica", 15, 20),
-  new Objet("Pass pour les coulisses d'un concert de Metallica", 10, 49),
-  new Objet("Pass pour les coulisses d'un concert de Metallica", 5, 49),
+  new ObjetRoseDoree("Veste de dexterite +5", 10, 20), //
+  new VieuxBrie("Vieux Brie", 2, 0), //
+  new ObjetRoseDoree("Elixir de la Mangouste", 5, 7), //
+  new Sulfura("Sulfuras, Main de Ragnaros", 0, 80), //
+  new Sulfura("Sulfuras, Main de Ragnaros", -1, 80),
+  new PassCoulisseMetallica("Pass pour les coulisses d'un concert de Metallica", 15, 20),
+  new PassCoulisseMetallica("Pass pour les coulisses d'un concert de Metallica", 10, 49),
+  new PassCoulisseMetallica("Pass pour les coulisses d'un concert de Metallica", 5, 49),
   // Cet objet Conjure ne fonctionne pas correctement pour le moment
-  new Objet("Gateau de Mana Conjure", 3, 6)];
+  new Conjure("Gateau de Mana Conjure", 3, 6)];
 
 
 const roseDoree = new RoseDoree(objets);
@@ -24,7 +30,7 @@ for (let i = 0; i < jours; i++) {
   console.log("-------- jour " + i + " --------");
   console.log("nom, joursRestantsAvantPeremption, qualite");
   objets.forEach(element => {
-    console.log(element.nom + ' ' + element.joursRestantsAvantPeremption + ' ' + element.qualite);
+    console.log(element.nom + ' / Péremption dans ' + element.joursRestantsAvantPeremption + ' jours / Qualité : ' + element.qualite);
 
   });
   console.log();
