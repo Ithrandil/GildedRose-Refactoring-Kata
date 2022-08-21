6c4230388bab3f50202e95398e0ce76fdef58217
Alexandre Guérin <alexandre.guerin17@gmail.com>
2022-08-21 16:18:30 +0200

    Test diminuer la 'qualité' de 2 si il n'y a plus de 'jours restants avant péremption'

--- TypeScript/test/jest/rose-doree.spec.ts
+++ TypeScript/test/jest/rose-doree.spec.ts
@@ -13,4 +13,9 @@ describe("Rose Doree", () => {
     const objets = roseDoree.miseAJourqualite();
     expect(objets[0].joursRestantsAvantPeremption).toBe(19);
   });
-});
+
+  it("Devrait diminuer la 'qualité' de 2 si il n'y a plus de 'jours restants avant péremption'", () => {
+    const roseDoree = new RoseDoree([new Objet("Bière", 0, 20)]);
+    const objets = roseDoree.miseAJourqualite();
+    expect(objets[0].qualite).toBe(18);
+  });
